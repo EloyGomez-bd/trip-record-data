e15d96f2269aa6c519ac5d8fe79a57c225361e74
Eloy Gomez <emoreno@outlook.dk>
2023-09-06 15:38:51 +0200

    Error handling

--- main.py
+++ main.py
@@ -32,17 +32,19 @@ schema = StructType([
     StructField("airport_fee", DoubleType())
 ])
 
-# Function to create a Spark session
+
 def create_spark_session(session):
-    """Function to create spark session and set loggin level"""
+
+    """Function to create spark session and set logging level"""
 
     spark = SparkSession.builder.appName(session).getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
     print(f"Spark session {session} created")
     return spark
 
-# Function to download Parquet files
+
 def download_parquet_files(start_date, end_date, download_path):
+
     """Function that downloads the Parquet files of TLC trip record data
     and stores them in a dedicated path for further processing"""
 
@@ -76,77 +78,94 @@ def download_parquet_files(start_date, end_date, download_path):
     except Exception as e:
         print(f"Error: {str(e)}")
 
+
 # Function to process Parquet data
 def process_parquet_data(folder_path):
     sdf_list = []
 
-    # Load the Parquet files from the local file system into a Spark DataFrame
-    files = [f for f in os.listdir(folder_path) if f.endswith(".parquet")]
-    for file_name in files:
-        file_path = os.path.join(folder_path, file_name)
-        # Read the Parquet file applying the custom schema
-        sdf = spark.read.parquet(file_path)
-        # Convert fields to our schema
-        sdf = sdf.withColumn("VendorID", col("VendorID").cast("bigint"))
-        # Add the DataFrame to the list
-        sdf_list.append(sdf)
+    try:
 
-    # Append the DataFrames in the list
-    final_sdf = sdf_list[0]
-    print("Appending parquet files")
-    for sdf in sdf_list[1:]:
-        final_sdf = final_sdf.union(sdf)
+        # Load the Parquet files from the local file system into a Spark DataFrame
+        files = [f for f in os.listdir(folder_path) if f.endswith(".parquet")]
 
-    final_sdf = final_sdf.orderBy(col("trip_distance").desc())
+        for file_name in files:
+            file_path = os.path.join(folder_path, file_name)
+            # Read the Parquet file applying the custom schema
+            sdf = spark.read.parquet(file_path)
+            # Convert fields to our schema
+            sdf = sdf.withColumn("VendorID", col("VendorID").cast("bigint"))
+            # Add the DataFrame to the list
+            sdf_list.append(sdf)
 
-    # Calculate 10% of the total and reassign the initial sdf
-    top_10per_rows = int(final_sdf.count() * 0.10)
-    final_sdf = final_sdf.limit(top_10per_rows)
+        # Append the DataFrames in the list
+        sdf = sdf_list[0]
+        print("Appending parquet files")
+        for sdf in sdf_list[1:]:
+            sdf = sdf.union(sdf)
 
-    return final_sdf
+        sdf = sdf.orderBy(col("trip_distance").desc())
+
+        # Calculate 10% of the total and reassign the initial sdf
+        top_10per_rows = int(final_sdf.count() * 0.10)
+        sdf = sdf.limit(top_10per_rows)
+
+        return sdf
+
+    except Exception as e:
+        print(f"Error processing parquet data: {str(e)}")
+        return None
+
 
 def data_quality_total_amount(sdf):
 
-    """
-        Check and correct data quality issues in the input DataFrame.
-    """
+    """ Check and correct data quality issues in the input DataFrame """
 
-    # Check for null values in the "total_amount" column
-    null_check = sdf.filter(col("total_amount").isNull())
+    try:
 
-    # Check for negative values in "total_amount" and correct other columns accordingly
-    negative_check = sdf.filter(col("total_amount") < 0)
+        # Check for null values in the "total_amount" column
+        null_check = sdf.filter(col("total_amount").isNull())
 
-    if not null_check.isEmpty():
-        print("Null values found in the 'total_amount' column.")
+        # Check for negative values in "total_amount" and correct other columns accordingly
+        negative_check = sdf.filter(col("total_amount") < 0)
 
-    if not negative_check.isEmpty():
-        print("Negative 'total_amount' values found. Correcting...")
+        if not null_check.isEmpty():
+            print("Null values found in the 'total_amount' column.")
 
-        # Define a list of columns to correct
-        columns_to_correct = [
-            "fare_amount",
-            "extra",
-            "mta_tax",
-            "tip_amount",
-            "tolls_amount",
-            "improvement_surcharge",
-            "total_amount"
-        ]
+        if not negative_check.isEmpty():
+            print("Negative 'total_amount' values found. Correcting...")
 
-        # Multiply selected columns by -1 for rows with negative "total_amount"
-        for column in columns_to_correct:
-            sdf = sdf.withColumn(column, when(col("total_amount") < 0, col(column) * -1).otherwise(col(column)))
+            # Define a list of columns to correct
+            columns_to_correct = [
+                "fare_amount",
+                "extra",
+                "mta_tax",
+                "tip_amount",
+                "tolls_amount",
+                "improvement_surcharge",
+                "total_amount"
+            ]
 
-    # Check data type of "total_amount" column
-    if sdf.schema["total_amount"].dataType != DoubleType():
-        print("The data type of 'total_amount' column is not DoubleType.")
+            # Multiply selected columns by -1 for rows with negative "total_amount"
+            for column in columns_to_correct:
+                sdf = sdf.withColumn(column, when(col("total_amount") < 0, col(column) * -1).otherwise(col(column)))
 
-    return sdf
+        # Check data type of "total_amount" column
+        if sdf.schema["total_amount"].dataType != DoubleType():
+            print("The data type of 'total_amount' column is not DoubleType.")
+
+        return null_check, negative_check, sdf
+
+    except Exception as e:
+
+        print(f"Error in data quality check: {str(e)}")
+        return None, None, None
+
 
 # Function to parse command line arguments
 def parser():
+
     """Function to parse arguments on the script"""
+
     parser = argparse.ArgumentParser(description="Download and process Yellow Taxi trip data.")
     parser.add_argument("start_date", help="Start date in yyyy-MM format")
     parser.add_argument("end_date", help="End date in yyyy-MM format")
@@ -157,9 +176,19 @@ def parser():
 if __name__ == '__main__':
 
     spark = create_spark_session("TripRecordData")
+
     args = parser()
+
     download_parquet_files(args.start_date, args.end_date, args.download_path)
+
     processed_data = process_parquet_data(args.download_path)
-    final_sdf = data_quality_total_amount(processed_data)
+
+    null_check, negative_check, final_sdf = data_quality_total_amount(processed_data)
+
     final_sdf.write.mode("overwrite").parquet('processed_data')
+
+    null_check.write.mode("overwrite").parquet('processed_data/null_data')
+
+    negative_check.write.mode("overwrite").parquet('processed_data/negative_amount')
+
     spark.stop()
