60f88a4aacde4dc4d8d3c4a09f2e821322200c4d
Eloy Gomez <emoreno@outlook.dk>
2023-09-06 15:17:01 +0200

    Data wrangling function

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import argparse
 import io
 import requests
 import os
+import shutil
 import pandas as pd
 import pyarrow.parquet as pq
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-from pyspark.sql.types import StructType, StructField, StringType, LongType, DoubleType, TimestampType, IntegerType
+from pyspark.sql.types import StructType, StructField, StringType, LongType, DoubleType, TimestampType
 
+# Define the schema for the Parquet files
 schema = StructType([
     StructField("VendorID", LongType()),
     StructField("tpep_pickup_datetime", TimestampType()),
@@ -30,21 +32,26 @@ schema = StructType([
     StructField("airport_fee", DoubleType())
 ])
 
+# Function to create a Spark session
 def create_spark_session(session):
+    """Function to create spark session and set loggin level"""
+
     spark = SparkSession.builder.appName(session).getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
     print(f"Spark session {session} created")
     return spark
 
-
+# Function to download Parquet files
 def download_parquet_files(start_date, end_date, download_path):
-
-    """Function that downloads the parquet file on TLC trip record data and store them in dedicated path to further
-    processing"""
+    """Function that downloads the Parquet files of TLC trip record data
+    and stores them in a dedicated path for further processing"""
 
     try:
 
         os.makedirs(download_path, exist_ok=True)
+
+        # Delete files from other script executions
+        shutil.rmtree(download_path)
 
         date_range = pd.date_range(start_date, end_date)
         download_urls = [
@@ -53,84 +60,106 @@ def download_parquet_files(start_date, end_date, download_path):
         ]
 
         for url in download_urls:
-
-            # Descarga el archivo Parquet desde la URL
-
+            # Download the Parquet file from the URL
             response = requests.get(url)
 
             if response.status_code == 200:
-
-            # Guarda en memoria y escribe a la local
-
+                # Save in memory and write to the local file
                 buffer_table = io.BytesIO(response.content)
                 table = pq.read_table(buffer_table)
-
                 local_file_path = os.path.join(download_path, os.path.basename(url))
-
                 pq.write_table(table, local_file_path)
-
+                print(f"Downloading {os.path.basename(url)}")
             else:
                 print(f"Failed to download data from {url}")
 
     except Exception as e:
         print(f"Error: {str(e)}")
 
-
+# Function to process Parquet data
 def process_parquet_data(folder_path):
-
     sdf_list = []
 
-    # Cargamos los archivos parquet desde el sistema de archivos local a un dataframe de spark
-
+    # Load the Parquet files from the local file system into a Spark DataFrame
     files = [f for f in os.listdir(folder_path) if f.endswith(".parquet")]
-
     for file_name in files:
         file_path = os.path.join(folder_path, file_name)
-
-        # Lee el archivo Parquet aplicando el esquema personalizado
+        # Read the Parquet file applying the custom schema
         sdf = spark.read.parquet(file_path)
-
-        # Convertimos campos a nuestro schema
-
+        # Convert fields to our schema
         sdf = sdf.withColumn("VendorID", col("VendorID").cast("bigint"))
-
-        # Agrega el DataFrame a la lista
+        # Add the DataFrame to the list
         sdf_list.append(sdf)
 
-    # Realiza un append de los DataFrames en la lista
+    # Append the DataFrames in the list
     final_sdf = sdf_list[0]
-
+    print("Appending parquet files")
     for sdf in sdf_list[1:]:
         final_sdf = final_sdf.union(sdf)
 
     final_sdf = final_sdf.orderBy(col("trip_distance").desc())
 
-    # Calculamos el 10% del total y reasignamos el sdf inicial
-
+    # Calculate 10% of the total and reassign the initial sdf
     top_10per_rows = int(final_sdf.count() * 0.10)
     final_sdf = final_sdf.limit(top_10per_rows)
 
     return final_sdf
 
+def data_quality_total_amount(sdf):
+
+    """
+        Check and correct data quality issues in the input DataFrame.
+    """
+
+    # Check for null values in the "total_amount" column
+    null_check = sdf.filter(col("total_amount").isNull())
+
+    # Check for negative values in "total_amount" and correct other columns accordingly
+    negative_check = sdf.filter(col("total_amount") < 0)
+
+    if not null_check.isEmpty():
+        print("Null values found in the 'total_amount' column.")
+
+    if not negative_check.isEmpty():
+        print("Negative 'total_amount' values found. Correcting...")
+
+        # Define a list of columns to correct
+        columns_to_correct = [
+            "fare_amount",
+            "extra",
+            "mta_tax",
+            "tip_amount",
+            "tolls_amount",
+            "improvement_surcharge",
+            "total_amount"
+        ]
+
+        # Multiply selected columns by -1 for rows with negative "total_amount"
+        for column in columns_to_correct:
+            sdf = sdf.withColumn(column, when(col("total_amount") < 0, col(column) * -1).otherwise(col(column)))
+
+    # Check data type of "total_amount" column
+    if sdf.schema["total_amount"].dataType != DoubleType():
+        print("The data type of 'total_amount' column is not DoubleType.")
+
+    return sdf
+
+# Function to parse command line arguments
 def parser():
-
     """Function to parse arguments on the script"""
-
     parser = argparse.ArgumentParser(description="Download and process Yellow Taxi trip data.")
     parser.add_argument("start_date", help="Start date in yyyy-MM format")
     parser.add_argument("end_date", help="End date in yyyy-MM format")
     parser.add_argument("--download_path", default="raw_data", help="Download path for Parquet files")
-
     return parser.parse_args()
+
 
 if __name__ == '__main__':
 
     spark = create_spark_session("TripRecordData")
-
     args = parser()
-
     download_parquet_files(args.start_date, args.end_date, args.download_path)
-
-    process_parquet_data(args.download_path)
-
+    processed_data = process_parquet_data(args.download_path)
+    final_sdf = data_quality_total_amount(processed_data)
+    final_sdf.write.mode("overwrite").parquet('processed_data')
     spark.stop()
